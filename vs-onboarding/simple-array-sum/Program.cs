c0ae6958b03ac018f93e618ca9f5d77986161c7e
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-23 18:04:11 +0000

    Adds solution for simple-array-sum

---
+++ vs-onboarding/simple-array-sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arrTemp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arrTemp, Int32.Parse);
+        int sum = arr.Sum();
+
+        Console.Out.Write(sum);
+    }
+}
