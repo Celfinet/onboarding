0a6dd00c8fc8d99b8090871b09c459b9cf1a18d9
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-24 09:39:41 +0000

    Adds utopian tree solution

---
+++ vs-onboarding/utopian-tree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int tree = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                tree = (i % 2 == 0 ? tree + 1 : tree * 2);
+            }
+            Console.WriteLine(tree);
+        }
+    }
+}
