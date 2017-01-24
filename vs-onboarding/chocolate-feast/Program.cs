9eb409b15f1a385e2c3bc8e98a813e070340cfb9
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-24 14:35:28 +0000

    Adds Chocolate Feast solution

---
+++ vs-onboarding/chocolate-feast/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        int t = ToInt32(ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            string[] tokens_n = ReadLine().Split(' ');
+            int n = ToInt32(tokens_n[0]);
+            int c = ToInt32(tokens_n[1]);
+            int m = ToInt32(tokens_n[2]);
+            int counter = n / c;
+            int w = counter;
+            while (w >= m)
+            {
+                var d = w / m;
+                var r = w % m;
+                w = r + d;
+                counter += d;
+            }
+            WriteLine(counter);
+        }
+    }
+}
