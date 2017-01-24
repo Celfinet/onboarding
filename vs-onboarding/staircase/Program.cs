7b31d5a914ede8a81fe96745c5049ad6738800d1
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-24 09:38:13 +0000

    Adds staircase solution

---
+++ vs-onboarding/staircase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(j < n - (i - 1) ? " " : "#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
