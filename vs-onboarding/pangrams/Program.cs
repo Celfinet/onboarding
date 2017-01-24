608941032868d3ce8f31ecf1cdaa8ad9d84345e0
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-24 10:45:54 +0000

    Adds pangrams solution

---
+++ vs-onboarding/pangrams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+internal class Solution
+{
+    private static void Main(string[] args)
+    {
+        string abc = "abcdefghijklmnopqrstuvwxyz";
+        var abcList = abc.ToList();
+        var charArr = Console.ReadLine().Trim(' ').ToLower().ToCharArray();
+        foreach (var item in charArr)
+        {
+            for (int i = 0; i < abcList.Count; i++)
+                if (abcList.Contains(item))
+                    abcList.Remove(item);
+            if (abcList.Count == 0)
+                break;
+        }
+        if (abcList.Count == 0)
+            Console.WriteLine("pangram");
+        else Console.WriteLine("not pangram");
+    }
+}
