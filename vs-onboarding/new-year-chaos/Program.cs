0ea4d5d23addfb718bd38f5ffcf1b4f81b88b948
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-25 14:56:20 +0000

    Updates New Year Chaos solution

--- vs-onboarding/new-year-chaos/Program.cs
+++ vs-onboarding/new-year-chaos/Program.cs
@@ -6,46 +6,61 @@ using static System.Convert;
 class Solution
 {
 
-    static void Main(String[] args)
+    static void Main(string[] args)
     {
-        int T = ToInt32(ReadLine());
+        var T = ToInt32(ReadLine());
         for (int a0 = 0; a0 < T; a0++)
         {
-            int n = ToInt32(ReadLine());
-            string[] q_temp = ReadLine().Split(' ');
-            int[] q = Array.ConvertAll(q_temp, Int32.Parse);
+            var n = ToInt32(ReadLine());
+            var q_temp = ReadLine().Split(' ');
+            var q = Array.ConvertAll(q_temp, Int32.Parse);
             var dic = new Dictionary<int, int>();
-            int bribes = 0;
-            //    for (int i = 0; i < q.Length - 1; i++)
-            //    {
-            //        int j = i + 1;
-            //        if (q[i] > q[j])
-            //        {
-            //            if (dic.ContainsKey(q[i]))
-            //            {
-            //                if (dic[q[i]] >= 2)
-            //                {
-            //                    WriteLine("Too chaotic");
-            //                    break;
-            //                }
-            //                dic[q[i]]++;
-            //            }
-            //            else dic.Add(q[i], 1);
-            //            Swap(q,i,j);
-
-            //        }
-
-            //    }
-            //}
+            var bribes = 0;
+            var flag = false;
+            while (bribes != -1 && flag == false)
+            {
+                flag = true;
+                for (int i = 0; i < q.Length - 1; i++)
+                {
+                    var j = i + 1;
+                    if (q[i] > q[j])
+                    {
+                        flag = false;
+                        if (dic.ContainsKey(q[i]))
+                        {
+                            if (dic[q[i]] >= 2)
+                            {
+                                bribes = -1;
+                                WriteLine("Too chaotic");
+                                break;
+                            }
+                            dic[q[i]]++;
+                        }
+                        else dic.Add(q[i], 1);
+                        Swap(q, i, j);
+                    }
+                }
+            }
             if (bribes != -1)
+            {
+                bribes = CountBribes(dic);
                 WriteLine(bribes);
+            }
         }
-
     }
-    private static void Swap(int[] q, int a, int b)
+    private static void Swap(int[] q, int i, int j)
     {
-        int aux = q[a];
-        q[a] = q[b];
-        q[b] = a;
+        var aux = q[i];
+        q[i] = q[j];
+        q[j] = aux;
+    }
+    private static int CountBribes(Dictionary<int, int> dic)
+    {
+        var aux = 0;
+        foreach (var item in dic)
+        {
+            aux += item.Value;
+        }
+        return aux;
     }
 }
