5d724362b9faae91a79d26732da3a825fb80eb35
Jorge Barbedo <jfbarbedo@gmail.com>
2017-01-26 09:42:01 +0000

    Adds TDD KATA exercise 1 and 2 solution

---
+++ vs-onboarding/Cfn.OnBoarding.TDD.StringCalculator.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using Cfn.OnBoarding.TDD.StringCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfn.OnBoarding.TDD.StringCalculator.UnitTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("",0)]
+        [Description("Test Add method when input is an empty string")]
+        public void AddTestEmptyString(string input,int expected)
+        {
+
+            //Arrange
+            var strCalculator = new StringCalculator();
+
+            //Act
+            var result = strCalculator.Add(input);
+            //Assert
+            Assert.AreEqual(expected,result);
+        }
+
+        [TestCase("1", 1)]
+        [Description("Test Add method when input is an one number string")]
+        public void AddTestOneNumberString(string input, int expected)
+        {
+
+            //Arrange
+            var strCalculator = new StringCalculator();
+
+            //Act
+            var result = strCalculator.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2", 3)]
+        [Description("Test Add method when input is a two number string")]
+        public void AddTestTwoNumberString(string input, int expected)
+        {
+
+            //Arrange
+            var strCalculator = new StringCalculator();
+
+            //Act
+            var result = strCalculator.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2,3,2", 8)]
+        [TestCase("4,2,1,5", 12)]
+        [TestCase("1,2,3,2,3,8,2", 21)]
+        [Description("Test Add method when input is an unknown amount of numbers string")]
+        public void AddTestUnkwonNumberString(string input, int expected)
+        {
+
+            //Arrange
+            var strCalculator = new StringCalculator();
+
+            //Act
+            var result = strCalculator.Add(input);
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ vs-onboarding/Cfn.OnBoarding.TDD.StringCalculator/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfn.OnBoarding.TDD.StringCalculator
+{
+    public class StringCalculator : IStringCalculator
+    {
+        public int Add(string number)
+        {
+            if (number.Length == 0)
+            {
+                return 0;
+            }
+
+            if (number.Length == 1)
+            {
+                return int.Parse(number);
+            }
+            return number.Split(',').ToList().ConvertAll(int.Parse).Aggregate(0, (i, acc) => acc += i);
+
+        }
+    }
+}
