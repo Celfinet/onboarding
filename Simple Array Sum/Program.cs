91ee84d21e7f4edfb5cd4bf93f2d14113b5550a6
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-03 16:57:17 +0100

    Simple Array Sum solution

---
+++ Simple Array Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length=0;
+
+            if(int.TryParse(Console.ReadLine(), out length))
+            {
+                string[] arrStr = Console.ReadLine().Split(' ');
+                int[] arrInt = Array.ConvertAll(arrStr, int.Parse);
+                int sum = 0;
+                for(int i = 0; i < length && i<arrInt.Length; i++)
+                {
+                    sum += arrInt[i];                    
+                }
+                Console.WriteLine(sum);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
