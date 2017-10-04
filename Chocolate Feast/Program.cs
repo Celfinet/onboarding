a808d9fd58a721f3c8707503e85df00e1f4f6e97
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 15:28:31 +0100

    Chocolate Feast

---
+++ Chocolate Feast/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chocolate_Feast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (int.TryParse(Console.ReadLine(), out int T))
+            {
+                int count = 0;
+                int[] results = new int[T];
+                do
+                {
+                    string[] input_values = Console.ReadLine().Split(' ');
+                    int budget = Convert.ToInt32(input_values[0]);
+                    int unitPrice = Convert.ToInt32(input_values[1]);
+                    int packageAmmount = Convert.ToInt32(input_values[2]);
+                    int unitsbought = budget / unitPrice;
+
+                    int chocolatesWithoutPackage = unitsbought / packageAmmount * packageAmmount;
+                    int chocolatesWithPackage = unitsbought % packageAmmount + unitsbought / packageAmmount;
+
+
+                    results[count] = chocolatesWithoutPackage + chocolatesWithPackage + chocolatesWithPackage/ packageAmmount;
+                    
+                    count++;
+                } while (count < T);
+
+                foreach (int result in results)
+                {
+                    Console.WriteLine(result);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
