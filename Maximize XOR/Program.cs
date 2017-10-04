76b237391c3f03841a0d93257cb228f1a5340896
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 15:50:02 +0100

    Maximize XOR Solution

---
+++ Maximize XOR/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximize_XOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int L = Convert.ToInt32(Console.ReadLine());
+            int R = Convert.ToInt32(Console.ReadLine());
+            
+            int max = 0;
+            for (int i = L; i < R-1; i++)
+            {
+                for (int j = i+1; j < R; j++)
+                {
+                    int xor = j ^ i;
+                    if (xor > max)
+                    {
+                        max = xor;
+                    }
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
