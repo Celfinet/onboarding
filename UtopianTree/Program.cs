a4eb7e9855ba308b9e73635acaca015088e619b5
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 13:30:21 +0100

    UtopianTree Solution

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder("");
+
+
+            if (int.TryParse(Console.ReadLine(), out int length))
+            {
+                int count = 0;
+                while (count < length)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int cycles))
+                    {
+                        int treeheight = 1;
+                        for(int i = 1; i <= cycles; i++)
+                        {
+                            if (i % 2 == 0)
+                            {
+                                treeheight += 1;
+                            }
+                            else
+                            {
+                                treeheight *=2;
+                            }
+                        }
+                        result.AppendLine(treeheight.ToString());
+                    }
+                    count++;
+                }
+            }
+            Console.Write(result);
+            Console.ReadLine();
+
+        }
+    }
+}
