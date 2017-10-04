6e4da0db65f3d8efc4661383baa6730c893b726d
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 17:51:26 +0100

    Sherlock and The Beast

---
+++ Sherlock and The Beast/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sherlock_and_The_Beast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            StringBuilder result = new StringBuilder("");
+            do
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+
+                int number5 = N, number3 = 0;
+                bool found=false;
+                while (!found && number5>0)
+                {
+                    if (number5 % 3 == 0 && number3 == 0 && number5 == N) found = true;
+                    if (number3 % 5 == 0 && number5 == 0 && number3 == N) found = true;
+                    if (number3 % 5 == 0 && number5 % 3 == 0) found = true;
+                    if (!found)
+                    {
+                        number5--;
+                        number3++;
+                    }
+                }
+                if (number5 == -1)
+                {
+                    result.AppendLine("-1");
+                }
+                else
+                {
+                    result.AppendFormat("{0}{1}\n", new string('5', number5), new string('3', number3));
+                }
+                count++;
+            } while (count < T);
+            Console.Write(result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
