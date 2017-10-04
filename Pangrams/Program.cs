f5f9fea85b378bc1ca98ffc51f789f8c188cb7cf
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 13:44:56 +0100

    Pangrams Solutions

---
+++ Pangrams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputStr;
+            do
+            {
+                inputStr = Console.ReadLine();
+                inputStr = inputStr.ToUpper();
+                bool notPangram = false;
+                for(int chr = 65; chr <= 90; chr++)
+                {
+                    if (inputStr.IndexOf(((char)chr))==-1)
+                    {
+                        notPangram = true;
+                    }
+                }
+                Console.WriteLine(notPangram? "Not pangram":"pangram");
+            } while (!inputStr.Equals("N"));
+        }
+    }
+}
