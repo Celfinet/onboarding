ed3ec6913b98fda6959e08824eec763933983b43
Fábio Campelo Carvalho <fabio.c.carvalho@celfinet.com>
2017-10-04 14:54:32 +0100

    Fibonacci Modified

---
+++ Fibonacci Modified/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+            int[] arrInt = Array.ConvertAll(arr, int.Parse);
+            switch (arrInt[2])
+            {
+                case 1:
+                    Console.WriteLine(arrInt[0]);
+                    break;
+                case 2:
+                    Console.WriteLine(arrInt[1]);
+                    break;
+                default:
+                    Console.WriteLine(FibonnaciModified(arrInt[0], arrInt[1], arrInt[2], 3).ToString());
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        private static int FibonnaciModified(int ti, int ti1, int n, int current)
+        {
+            if (current == n) return ti + ti1 * ti1;
+            return FibonnaciModified(ti1, ti + ti1 * ti1, n, current + 1);
+        }
+    }
+}
